91076e02f4b4f46505cee5f8665f141b944400a7
JustAgent <baykov.ivan.03@gmail.com>
2022-09-20 00:18:17 +0300

    listeners done

--- src/frontend/components/Create/create_page.js
+++ src/frontend/components/Create/create_page.js
@@ -9,20 +9,17 @@ const CreateNftPage = () => {
     const [supply, setsupply] = useState()
 
     const nameListener = (text) => {
-        const temp = text.target.value
-        setname(temp)
-        console.log(name);
+        setname(text.target.value)
     
     }
     const tagListener = (text) => {
-        settag(text)
+        settag(text.target.value)
     }
     const descListener = (text) => {
-        setdesc(text)
+        setdesc(text.target.value)
     }
     const supplyListener = (text) => {
-        setsupply(parseInt(text))
-        console.log(supply);
+        setsupply(parseInt(text.target.value))
 
     }
     
@@ -37,7 +34,7 @@ const CreateNftPage = () => {
                 <InputFormModel name="Tag" desc='Short name of your collection' callback={tagListener} />
                 <InputFormModel name="Description" desc='The description will be included on the item detail page underneath its image.' callback={descListener} />
                 <InputFormModel name="Supply" desc='The number of items that can be minted.' callback={supplyListener} />
-                <button>Submit</button>
+                <button onClick={() => console.log(name, tag, desc, supply)}>Submit</button>
             </div>
         </div>
      );
